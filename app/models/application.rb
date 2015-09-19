bc60058c93a91a1f1f0abedef30e92adafc6692c
Sheri Wilson <smgwilson37@gmail.com>
2015-09-18 17:45:58 -0700

    Aliases model field names

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,17 +1,17 @@
 class Application
   include Mongoid::Document
-  field :app_name, type: String
-  field :app_company, type: String
-  field :app_icon, type: String
-  field :app_url, type: String
-  field :rating, type: Float
-  field :price, type: Float
-  field :global_rating_cnt, type: Integer
-  field :country_rating_cnt, type: Integer
-  field :crrnt_vrsn_rating_cnt, type: Integer
-  field :iphone_dwnld_cnt, type: Integer
-  field :ipad_dwnld_cnt, type: Integer
-  field :inapp_purchase, type: Mongoid::Boolean
-  field :iphone_revenue, type: String
-  field :ipad_revenue, type: String
+  field :an, as: :app_name, type: String
+  field :ac, as: :app_company, type: String
+  field :ai, as: :app_icon, type: String
+  field :au, as: :app_url, type: String
+  field :r, as: :rating, type: Float
+  field :p, as: :price, type: Float
+  field :grc, as: :global_rating_cnt, type: Integer
+  field :crc, as: :country_rating_cnt, type: Integer
+  field :cvrc, as: :acrrnt_vrsn_rating_cnt, type: Integer
+  field :phdc, as: :iphone_dwnld_cnt, type: Integer
+  field :idc, as: :ipad_dwnld_cnt, type: Integer
+  field :ip, as: :inapp_purchase, type: Mongoid::Boolean
+  field :phr, as: :iphone_revenue, type: String
+  field :ir, as: :ipad_revenue, type: String
 end
