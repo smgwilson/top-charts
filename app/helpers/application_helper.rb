64d3f9cef8d96f9d3852fc062bafb657b1ebc7ac
Sheri Wilson <smgwilson37@gmail.com>
2015-09-21 19:00:47 -0700

    Star ratings working - woohoogit add .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,40 @@ module ApplicationHelper
     a.iphone_dwnld_cnt ||= 0 + a.ipad_dwnld_cnt ||= 0
   end
 
+  def app_icon(a)
+    a.app_icon
+  end
+
+  def app_ratings (num, a)
+    max_stars = 5
+    star_array = []
+    half_stars = 0
+    on_stars = 0
+    
+    if a == 0
+      off_stars = max_stars
+    elsif a < 1
+      half_stars = 1
+      off_stars = max_stars - half_stars
+    else
+      on_stars = a.floor
+      a % on_stars == 1 ? half_stars = 0 : half_stars = 1
+      off_stars = (max_stars - (on_stars + half_stars))
+    end
+
+    on_stars.times do
+      star_array <<  'star-on.png'
+    end
+
+    half_stars.times do
+      star_array <<  'star-half.png'
+    end
+
+    off_stars.times do
+      star_array <<  'star-off.png'
+    end
+
+    return star_array[num]
+  end
+
 end
--- app/helpers/top_paid_helper.rb
+++ app/helpers/top_paid_helper.rb
@@ -1,7 +1,2 @@
 module TopPaidHelper
-
-  def app_icon(a)
-    a.app_icon
-  end
-
 end
