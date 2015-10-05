de3b31075fcc67c7ebd19571ed887c8669dd8ff3
Sheri Wilson <smgwilson37@gmail.com>
2015-10-05 13:19:17 -0700

    experiment with eliminating calls to paid scope

--- app/controllers/top_paid_controller.rb
+++ app/controllers/top_paid_controller.rb
@@ -6,7 +6,7 @@ limited to 400
 
 class TopPaidController < ApplicationController
   def index
-    @paid = Application.paid.sorted_by_download_count.paginate(:page => params[:page], :per_page => 50)
+    @paid = Application.free.sorted_by_download_count.paginate(:page => params[:page], :per_page => 50)
   end
 
 end
